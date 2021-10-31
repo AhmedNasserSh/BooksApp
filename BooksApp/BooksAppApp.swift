8cf707e4d85768e4fa421d646b06dca573488154
Ahmed Naser <ahmed.nasser2310@gmail.com>
2021-10-31 19:09:25 +0200

    add project files

---
+++ BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Ahmed Naser on 31/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
